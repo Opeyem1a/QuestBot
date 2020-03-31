4422ef3fffa7b87e915120359c214dd2170517d1
opeyem1a <opeyadeyemi@gmail.com>
2020-03-31 00:29:57 -0700

    Ensuring checks for blank spaces of black cards and incorrect type inputs

--- Commands/CAHplay.js
+++ Commands/CAHplay.js
@@ -8,25 +8,40 @@ function validate(args){
   return true;
 };
 
-function verifyBlack(args) {
+function verifyBlack(message, args) {
   //args have had the command statement removed by this point, stil in array form
-  if(args.includes("///")) return true;
+
+  //counts the occurrences of
+  var count = args.reduce((p, item) => {
+      return (item === '///') ? p + 1 : p;
+    }, 0);
+  if(count == 0) {
+    message.channel.send(`${f.bold(message.author.username)}, you didn't include the "///" character to tell me where the blanks are!`);
+  } else if(count > 1) {
+    message.channel.send(`${f.bold(message.author.username)}....yeah let's hold off with the multi-blank cards for now, coming ${f.bold('soon')}™ though!`);
+  } else {
+    return true;
+  }
+  //return false if there isn't exactly 1 blank
+  return false;
 }
 
 function cardExists(cards, type, args){
   var exists = false;
   if(type === 'w') {
-    for(var w in cards.whitecards){
-      console.log('Check');
-      exists = exists || (w.equalsIgnoreCase(args.join(" ")));
+    for(var w of cards.whitecards){
+      //compare ignoring case and accents
+      exists = exists || (w.localeCompare(args.join(" "), undefined, { sensitivity: 'base' }) === 0);
     }
   } else if(type === 'b') {
     for(var b in cards.blackcards){
-      exists = exists || (b.equalsIgnoreCase(args.join(" ")));
+      //compare ignoring case and accents
+      exists = exists || (b.localeCompare(args.join(" "), undefined, { sensitivity: 'base' }) === 0);
     }
   }
   return exists;
 }
+
 module.exports = {
   execute(client, message, args) {
     //if arguments are valid
@@ -42,8 +57,14 @@ module.exports = {
           }
           const cards = require('../Classes/CAHCards.json');
           if(cardExists(cards, type, args)){
-
+            message.channel.send(`${f.bold(message.author.username)}, it looks like this card has already been added, sorry! :'(`);
           } else {
+            if(type === 'w') cards.whitecards.push(args.join(" "));
+            else {
+              //for black cards, we check that '///' is included as a blank section
+              if(verifyBlack(message, args)) cards.blackcards.push(args.join(" "));
+              else return;
+            }
             fs.writeFile('./Classes/CAHCards.json', JSON.stringify(cards, null, 2), (error) => {
               if (error) console.error(error);
             });
