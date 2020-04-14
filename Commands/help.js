83543b4e7659059b540b8978eb32f2caafbf322c
opeyem1a <opeyadeyemi@gmail.com>
2020-04-13 19:44:46 -0600

    help.js now responds with a Discord message embed

--- Commands/help.js
+++ Commands/help.js
@@ -18,7 +18,7 @@ function addEmbedField(embed, command){
 }
 
 function commandEmbed(embed, command){
-  embed.setTitle(command.name);
+  embed.setTitle(f.capitalize(command.name));
 
   var params = command.usage ? f.italics(command.usage) : "";
   var usage = `${config.prefix}${command.name} ${params}`;
@@ -33,51 +33,6 @@ function commandEmbed(embed, command){
 
   embed.addField(`Description`, command.description, false);
   return embed;
-}
-
-function campaignEmbed(campaign){
-
-}
-
-//returns a help string for a command given an array containing the information needed
-function displayCommand(items){
-  var output = "";
-  //if this is for a bulk commands view print.
-  if(items.length == 3) {
-    output = `${config.prefix}${items[0]}`;
-    //if there is no usage parameter, place this in it's position instead
-    var usage = items[2] ? f.code(items[2]) : "";
-    output = `${f.bold(output)} ${usage} \n${f.italics(items[1])}`;
-  } else { //if a specific command was given
-    output = `${config.prefix}${items[0]}`;
-    //format the command with the correct prefix
-    output = `${f.bold(f.code(output))}`;
-    //if there is no usage parameter, place these lines in it's position instead
-    var usage = items[2] ? f.code(items[2]) : "";
-    //if there are alias names for this command, concatenate them here and add to output
-    if(items[3] != 0) {
-      //format list of aliases: i.e. Alias1 | Alias2 | etc
-      var aliases = items[3].reduce((a, b) => {
-        b = f.code(`'${b}'`);
-        if(a){ return `${a} ${b}`; } else {return b}
-      },"");
-      aliases = `Aliases: ${aliases}`;
-      output = `${output} ${usage} \n${aliases} \n${f.italics(items[1])}`;
-    } else {
-      //if no aliases, skip this section of the output entirely
-      output = `${output} ${usage} \n${items[1]}`;
-    }
-  }
-  return output;
-}
-
-//returns a help string for a campaign given an array containing the information needed
-function displayCampaign(items){
-  var output = "";
-  //format the string and capitalize the Name of the campaign properly
-  var numTasks = items[2].length == 1 ? items[2].length + ' Task' : items[2].length + ' Tasks';
-  output += `${f.bold(f.capitalize(items[0]))} - ${f.code(numTasks)} \n${items[1]}`;
-  return output;
 }
 
 module.exports = {
@@ -106,17 +61,11 @@ module.exports = {
             //find this specific command in the client's commands
             command = client.commands.get(args[0]);
             response = commandEmbed(embed, command);
-          // if a campaign was specified in the help call
-          } else if(client.campaigns.has(args[0].toLowerCase())){
-            //find this specific campaign in the client's campaign's
-            campaign = client.campaigns.get(args[0]);
-            //push relevant information from the campaign
-            items.push(campaign.name, campaign.synopsis, campaign.tasks);
-            response = displayCampaign(items);
-          } else {
+          }  else {
             //if the argument matches no commands or campaigns, return a default message
             response = responses.unfoundItem;
           }
+
           //message the help box for the required scenario in the chat
           message.channel.send(response);
         } catch (error) {
@@ -133,5 +82,5 @@ module.exports = {
   botPerms: [],
   name: "help",
   description: "Guide for bot commands overall or individually.",
-  usage: "[Command | Campaign]"
+  usage: "[command]"
 };
