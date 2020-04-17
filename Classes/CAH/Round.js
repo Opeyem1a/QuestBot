0de31c276a0dd0029507d71e8d1f3acffd4b92ee
opeyem1a <opeyadeyemi@gmail.com>
2020-04-17 02:05:10 -0600

    Bot now waits for the round owner to reveal everyone's card submissions and timesout if this process takes too long

--- Classes/CAH/Round.js
+++ Classes/CAH/Round.js
@@ -9,8 +9,9 @@ module.exports = class Round {
     this.client = client;
     this.message = message;
     this.ownerID = ownerID;
-    this.players = players
+    this.players = players;
     this.blackcard = blackcard;
+    this.owner = null;
 
     this.playedcards = new Map();
   }
@@ -21,13 +22,47 @@ module.exports = class Round {
 
   run() {
     //display the in-play black card to all players
-    this.players.forEach(values => {
-      const newEmbed = new Discord.MessageEmbed(values[1].embeds[0])
-         .setDescription(`Card in play: ${this.blackcard}`);
+    var start = new Promise((resolve, reject) => {
+      this.owner = this.players.get(this.ownerID)[0];
+      resolve("Starting Round");
+    })
+    start.then(() => {
+      this.players.forEach(player => {
+        if(player[0].id == this.ownerID) {
+          const newEmbed = new Discord.MessageEmbed(/*player[1].embeds[0]*/)
+              .setTitle(`Your Turn!`)
+              .setDescription(`Card in play: ${this.blackcard}`)
+              .addField(`Your Hand`, )
+              .setFooter(`${player[0].username}'s Hand`);
+        } else {
+          const newEmbed = new Discord.MessageEmbed(player[1].embeds[0])
+              .setTitle(`${this.owner.username}'s Turn!`)
+              .setDescription(`Card in play: ${this.blackcard}`)
+              .setFooter(`${player[0].username}'s Hand`);
+        }
+      });
+      player[1].edit(newEmbed).catch(e => console.error(e))
+    }).then(() => {
+      const filter = m => {return m.content.toLowerCase().startsWith('reveal') && m.author.id === this.ownerID;};
+      this.owner.dmChannel.awaitMessages(filter, { max: 1, time: 200000, errors: ['time'] })
+          .then(() => {
+            this.ownerChoose();
+          })
+          .catch(() => {
+            this.ownerTimeout();
+          });
+    })
 
-      values[1].edit(newEmbed).catch(e => console.error(e))
-    });
+    .catch(error => console.error(error));
+  }
 
+  ownerTimeout(){
+    //owner didn't choose to reveal in time, tell them this
+    this.ownerChoose();
+  }
+
+  ownerChoose(){
+    console.log(`${this.owner.username} Revealed!`);
   }
 
   playCard(cardNum, player, blackcard) {
